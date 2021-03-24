d53c63bb48284d4fa04e09b5be55c19c11f9c7cb
Nicosqui <nico50829@gmail.com>
2021-03-23 20:51:29 -0500

    first commit

---
+++ Prime_number/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Es " + number + " un número primo? " + IsPrime(number));
+        }
+
+        static bool IsPrime(int n)
+        {
+            return IsPrime(n, 2);
+        }
+        static bool IsPrime(int n, int i)
+        {
+            // i is supposed to start as 2.
+            if (n % i == 0)
+            {
+                return false;
+            }
+            else if (i >= n / 2)
+            {
+                return true;
+            }
+            else
+            {
+                return IsPrime(n, i + 1);
+            }
+        }
+    }
+}
